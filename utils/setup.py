7b753cd7d876a249b14f8edffa525adc6639ed36
Claudio Cambra <developer@claudiocambra.com>
2023-06-29 16:35:16 +0800

    Add set_device convenience setup helper in setup.py
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- utils/setup.py
+++ utils/setup.py
@@ -6,6 +6,14 @@ import torch
 import random
 
 
+def set_device() -> torch.device:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
+    print('Running on device: ', device)
+    return device
+
+
 def set_seed(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
