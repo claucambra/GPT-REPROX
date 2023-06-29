09fe93501480f50f6695b1593e5a4cf7d913a155
Claudio Cambra <developer@claudiocambra.com>
2023-06-29 14:30:57 +0800

    Add starter conversation system and human prompts
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- minedojo_minigpt/conversation.py
+++ minedojo_minigpt/conversation.py
@@ -4,9 +4,19 @@
 from ..minigpt_utils import ConversationWithImages
 
 
+MINEDOJO_TASKPROMPT = "Give the following image: <Img>ImageContent</Img>. " \
+                      "You will be provided with images of the video game Minecraft. " \
+                      "These images will show the player attempting to complete a task. " \
+                      "You will be told what task the player is attempting to complete. " \
+                      "You will then be asked to provide an integer score ranging from 0-100 based on how close you think the player is to completing the task. " \
+                      "Use the provided images to formulate the score. " \
+                      "Please respond only with a numerical score."
+
+CURRENT_STATE_PROMPT = "Generate a score from 0-100 based on how close you think the player is to completing the task. "
+
 class MineDojoMiniGPT4Conversation(ConversationWithImages):
     def __init__(self, 
-                 system: str, 
+                 system: str = MINEDOJO_TASKPROMPT, 
                  roles: tuple = ("Human", "Assistant"), 
                  messages: list = [], 
                  offset: int = 2, 
