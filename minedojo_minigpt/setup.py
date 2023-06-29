de3414eb054be27dea1a0b6085d8ec4e3662fede
Claudio Cambra <developer@claudiocambra.com>
2023-06-29 17:50:45 +0800

    Add helper function for setting up the minedojo gpt environment
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ minedojo_minigpt/setup.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2023 Claudio Cambra <developer@claudiocambra.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import argparse
+import os
+import torch
+
+from .env import MineDojoMiniGPT4Env
+from .task_config import setup_task_conf
+
+
+def setup_minedojo_gpt_env(args: argparse.Namespace, device: torch.device) -> MineDojoMiniGPT4Env:
+    save_dir = os.path.join(args.save_results_dir, args.task)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    task_conf = setup_task_conf(args.task)
+
+    return MineDojoMiniGPT4Env(
+        cmd_args=args,
+        seed=args.seed,
+        device=device,
+        save_rgb=0,
+        **task_conf)
