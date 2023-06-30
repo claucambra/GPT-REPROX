dd185d80986fcaf8030f8beacd27cb0b7278b72b
Claudio Cambra <developer@claudiocambra.com>
2023-06-30 18:02:58 +0800

    Only convert old gym text space if it is available
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- utils/gym_compat.py
+++ utils/gym_compat.py
@@ -6,6 +6,24 @@ from typing import Any, Callable
 from gym import spaces as old_spaces
 from gymnasium import spaces
 
+"""
+Some widely-used versions of gym did not come with the Text space type.
+We therefore selectively define a converter for the old Text type if it is
+available.
+"""
+try:
+    from gym.spaces import Text as _
+
+    __old_gym_space_text_available = True
+
+    def convert_gym_text(old_gym_text: old_spaces.Text) -> spaces.Text:
+        return spaces.Text(max_length=old_gym_text.max_length,
+                           min_length=old_gym_text.min_length,
+                           charset=old_gym_text.charset)
+except ImportError:
+    __old_gym_space_text_available = False
+    print("This version of gym does not have the Text space.")
+
 
 def convert_gym_space_multidiscrete(old_gym_multidescrete: old_spaces.MultiDiscrete) -> spaces.MultiDiscrete:
     return spaces.MultiDiscrete(old_gym_multidescrete.nvec,
@@ -14,12 +32,6 @@ def convert_gym_space_multidiscrete(old_gym_multidescrete: old_spaces.MultiDiscr
 
 def convert_gym_space_discrete(old_gym_discrete: old_spaces.Discrete) -> spaces.Discrete:
     return spaces.Discrete(old_gym_discrete.n)
-
-
-def convert_gym_text(old_gym_text: old_spaces.Text) -> spaces.Text:
-    return spaces.Text(max_length=old_gym_text.max_length,
-                       min_length=old_gym_text.min_length,
-                       charset=old_gym_text.charset)
 
 
 def convert_gym_space_box(old_gym_box: old_spaces.Box) -> spaces.Box:
@@ -45,13 +57,14 @@ def convert_gym_space_with_fallback_callback(old_gym_space: old_spaces.Space,
         return convert_gym_space_dict(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.Box):
         return convert_gym_space_box(old_gym_space)
-    elif isinstance(old_gym_space, old_spaces.Text):
-        return convert_gym_text(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.Discrete):
         return convert_gym_space_discrete(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.MultiDiscrete):
         return convert_gym_space_multidiscrete(old_gym_space)
-    else:  # Don't know, so invoke the callback
+    elif __old_gym_space_text_available and isinstance(old_gym_space, old_spaces.Text):
+        return convert_gym_text(old_gym_space)
+    else:
+        # Don't know, so invoke the callback
         return callback(old_gym_space)
 
 
