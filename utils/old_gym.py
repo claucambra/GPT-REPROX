31c8c042a5f965451744c68c94fad7281b3ab521
Claudio Cambra <developer@claudiocambra.com>
2023-06-30 17:00:12 +0800

    Separate space discrete conversion into own func
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- utils/old_gym.py
+++ utils/old_gym.py
@@ -10,6 +10,10 @@ def convert_gym_space_multidiscrete(old_gym_multidescrete: old_spaces.MultiDiscr
                                 dtype=old_gym_multidescrete.dtype)
 
 
+def convert_gym_space_discrete(old_gym_discrete: old_spaces.Discrete) -> spaces.Discrete:
+    return spaces.Discrete(old_gym_discrete.n)
+
+
 def convert_gym_text(old_gym_text: old_spaces.Text) -> spaces.Text:
     return spaces.Text(max_length=old_gym_text.max_length,
                        min_length=old_gym_text.min_length,
@@ -41,7 +45,7 @@ def convert_gym_space(old_gym_space: old_spaces.Space) -> spaces.Space:
     elif isinstance(old_gym_space, old_spaces.Text):
         return convert_gym_text(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.Discrete):
-        return spaces.Discrete(old_gym_space.n)
+        return convert_gym_space_discrete(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.MultiDiscrete):
         return convert_gym_space_multidiscrete(old_gym_space)
     else:
