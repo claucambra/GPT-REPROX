b941ea1494321472129225a842596ed3d34db4a6
Claudio Cambra <developer@claudiocambra.com>
2023-06-30 16:58:13 +0800

    Separate space text conversion into own func
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- utils/old_gym.py
+++ utils/old_gym.py
@@ -10,6 +10,12 @@ def convert_gym_space_multidiscrete(old_gym_multidescrete: old_spaces.MultiDiscr
                                 dtype=old_gym_multidescrete.dtype)
 
 
+def convert_gym_text(old_gym_text: old_spaces.Text) -> spaces.Text:
+    return spaces.Text(max_length=old_gym_text.max_length,
+                       min_length=old_gym_text.min_length,
+                       charset=old_gym_text.charset)
+
+
 def convert_gym_space_box(old_gym_box: old_spaces.Box) -> spaces.Box:
     return spaces.Box(low=old_gym_box.low,
                         high=old_gym_box.high,
@@ -33,8 +39,7 @@ def convert_gym_space(old_gym_space: old_spaces.Space) -> spaces.Space:
     elif isinstance(old_gym_space, old_spaces.Box):
         return convert_gym_space_box(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.Text):
-        val_length = old_gym_space.shape[0]
-        return spaces.Text(min_length=val_length, max_length=val_length)
+        return convert_gym_text(old_gym_space)
     elif isinstance(old_gym_space, old_spaces.Discrete):
         return spaces.Discrete(old_gym_space.n)
     elif isinstance(old_gym_space, old_spaces.MultiDiscrete):
