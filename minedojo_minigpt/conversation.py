7f395b03373484631de14c84ce212023c7be434a
Claudio Cambra <developer@claudiocambra.com>
2023-06-30 22:32:09 +0800

    Elaborate on initial system prompt for MineDojo MiniGPT
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- minedojo_minigpt/conversation.py
+++ minedojo_minigpt/conversation.py
@@ -4,13 +4,34 @@
 from utils.conversation_images import ConversationWithImages
 
 
-MINEDOJO_TASKPROMPT = "Give the following image: <Img>ImageContent</Img>. " \
-                      "You will be provided with images of the video game Minecraft. " \
-                      "These images will show the player attempting to complete a task. " \
-                      "You will be told what task the player is attempting to complete. " \
-                      "You will then be asked to provide an integer score ranging from 0-100 based on how close you think the player is to completing the task. " \
-                      "Use the provided images to formulate the score. " \
-                      "Please respond only with a numerical score."
+MINEDOJO_TASKPROMPT = """
+You will be provided with images of the video game Minecraft.
+These images are of unmodded Minecraft being played in survival mode from the player's perspective.
+These images will show the player attempting to complete a task.
+The images are in sequential order, with the last image provided being the latest.
+Firstly, you must identify and remember the objects within the image.
+You will be asked to provide an integer score ranging from 0 to 100 based on how close the player is to completing a task.
+You will be told what task the player is trying to complete.
+Use the provided images to formulate the score.
+
+Your reply must follow these rules:
+
+Your reply must be strictly a number between 0 and 100 inclusive.
+
+If within the image the location, objects, and player actions are not at all related to the task, you must reply only with the number 0.
+If the player is performing a different task, or you cannot see any relevant information in the image, you must also reply only with the number 0.
+
+If within the image the location, objects, player actions or some combination are related to or  are relevant to the task, provide a score from 10 to 80.
+This score must be based on the relevance of the image to performing this task.
+Images with a more direct relevance to the task must get a higher score.
+
+If within the image all of the location, objects and actions are related to this task, give a score from 80 to 100 based on how complete the task appears to be.
+
+When assesing these scores, do so with reference to the rules and mechanics of traditional, unmodified, survival mode Minecraft.
+
+You must exclusively provide a numerical response.
+There must be no description, explanation, or reasoning included in your answer.
+"""
 
 CURRENT_STATE_PROMPT = "Generate a score from 0-100 based on how close you think the player is to completing the task. "
 
